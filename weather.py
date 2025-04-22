43e3df87a417704dd54a3e9bcd3fca20eb824f82
Ashutosh Bhardwaj <197ashutosh@gmail.com>
2025-04-22 17:20:13 +0530

    Initial commit

---
+++ weather.py
@@ -0,0 +1,29 @@
+import requests
+from plyer import notification
+
+API_KEY = "16c9e6bfb32acd99c6da28fed1e71966"  
+
+CITY = "Greater Noida"
+
+
+URL = f"http://api.openweathermap.org/data/2.5/weather?q={CITY}&appid={API_KEY}&units=metric"
+
+response = requests.get(URL)
+data = response.json()
+
+if data["cod"] == 200:
+    weather = data["weather"][0]["description"].capitalize()
+    temperature = data["main"]["temp"]
+    humidity = data["main"]["humidity"]
+
+
+    message = f"Weather: {weather}\nTemperature: {temperature}°C\nHumidity: {humidity}%"
+
+   
+    notification.notify(
+        title=f"Weather Update - {CITY}",
+        message=message,
+        timeout=10
+    )
+else:
+    print(f"Error fetching weather data: {data}")
---
+++ weather_notification.bat
@@ -0,0 +1,3 @@
+@echo off
+pythonw "C:\Users\197as\OneDrive\Documents\Data_Science\weather.py"
+exit
